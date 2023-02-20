deec836de4d9a613e5bf69bd76e39044221ab5ce
Vikram Jain <jainvikram444@gmail.com>
2023-02-21 02:21:27 +0530

    Final code

--- main.go
+++ main.go
@@ -1,31 +1,20 @@
 package main
 
 import (
-	"fmt"
 	"go-chart-video-audio-multiple-peer/server"
 	"log"
 	"net/http"
 )
 
 func main() {
-	const (
-		httpPort string = ":3000"
-	)
-	server.Rooms.Init()
+	server.AllRooms.Init()
 
-	fmt.Println("Welcome to WebRTC peer to peeer video call between the muliple users.")
+	http.HandleFunc("/create", server.CreateRoomRequestHandler)
+	http.HandleFunc("/join", server.JoinRoomRequestHandler)
 
-	http.HandleFunc("/create_room", server.CreateRoomRequest)
-	http.HandleFunc("/join_room", server.JoinRoomRequest)
-
-	log.Println("Initialize the Server")
-
-	log.Printf("Server successfully running on port: %v", httpPort)
-	errHttp := http.ListenAndServe(httpPort, nil)
-	if errHttp != nil {
-		fmt.Println("Error: some issue in starting server: ", errHttp)
-		log.Fatal(errHttp)
-		return
+	log.Println("Starting Server on Port 8000")
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Fatal(err)
 	}
-	log.Printf("Server successfully stop on port: %v", httpPort)
 }
